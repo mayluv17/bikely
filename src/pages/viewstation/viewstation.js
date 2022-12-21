2891ea57a3a78ee963264f5a3d2892a909f5b60a
“mayluv17” <mayowaibrahim009@gmail.com>
2022-12-21 01:11:20 +0100

    "created station view with google map API"

--- src/pages/viewstation/viewstation.js
+++ src/pages/viewstation/viewstation.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Header from "../../components/header/Header";
 import "./viewstation.css";
-import { GoogleMap, useLoadeScript, Marker } from "google-map-react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 // AIzaSyABZHUfZhWdj8YJ6LW48PJhzd_7f8xulUg;
 
 export default function ViewStation() {
-  console.log(process.env.REACT_APP_GOOGLE_MAP_API);
+  //   console.log(process.env.REACT_APP_GOOGLE_MAP_API);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
+  });
+
   return (
     <>
       <Header />
-      <div className="map-area"></div>
+      <div className="map-area">{!isLoaded ? "Loading..." : <Map />}</div>
       <div className="details-wrap grid">
         <div className="name-wrap">
           <h6>Name</h6>
@@ -30,5 +34,13 @@ export default function ViewStation() {
         </div>
       </div>
     </>
+  );
+}
+function Map() {
+  const location = useMemo(() => ({ lat: 60.184312, lng: 24.826671 }), []);
+  return (
+    <GoogleMap zoom={10} center={location} mapContainerClassName="map-area">
+      <Marker position={location} />
+    </GoogleMap>
   );
 }
